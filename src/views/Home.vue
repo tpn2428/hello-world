e83483e87ebc38e17b8b74fc0a8bb6d9ca189e19
Thu Nguyen <nguyenphamanhthu2508@gmail.com>
2020-05-16 23:37:50 -0400

    setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,14 +5,4 @@
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
-</script>
+<script src="./home.vue.ts"></script>
